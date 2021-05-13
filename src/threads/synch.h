3e70aafc33034ee24cd67b0ffe4f2b86e568d631
Richard <phanr@lafayette.edu>
2021-05-13 17:05:46 -0400

    Deleted an unecessary attribute in lock

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,7 +22,6 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
-    struct list_elem lock_elem;  //Temporary fix will fix later
     bool *donated;
     int priority // The priority at which the thread was at when the lock was locked
   };
